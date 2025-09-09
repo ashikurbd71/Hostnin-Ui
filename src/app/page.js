e652c9d79dcd090129e6fff8b0d5cc9988fa176d
ashikurovibd71 <ashikurovi2003@gmail.com>
2025-09-09 20:44:35 +0600

    -update

--- src/app/page.js
+++ src/app/page.js
@@ -2,16 +2,21 @@ import ControlPanel from "@/components/home/ControlPanel";
 import HappyCustomer from "@/components/home/HappyCustomer";
 import HaveQuestions from "@/components/home/HaveQuestions";
 import Hero from "@/components/home/hero";
-import "slick-carousel/slick/slick.css"; 
+import Hostingplan from "@/components/home/Hostingplan";
+import OntherHost from "@/components/home/OntherHost";
+import SearchDomain from "@/components/home/SearchDomain";
+import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 export default function Home() {
   return (
     <div>
       <Hero />
-
+      <Hostingplan />
+      <OntherHost />
+      <SearchDomain />
       <ControlPanel />
-      <HaveQuestions/>
-      <HappyCustomer/>
+      <HaveQuestions />
+
     </div>
   );
 }
